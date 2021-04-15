b2c2ffcb31777bfa572aa7e86c58f394d0fa8bc9
Disha Chhabra <72024708+DishaChhabra@users.noreply.github.com>
2021-04-15 12:14:50 +0530

    Add files via upload

---
+++ drops.js
@@ -0,0 +1,23 @@
+class Drop{
+    constructor(x,y){
+        var options = {
+            friction: 0.1,
+            restitution: 0.1
+        }
+    this.body = Bodies.circle(x,y,5)
+    this.x = x
+    this.y = y
+    
+    World.add(world, this.body)
+    }
+    display(){
+        fill("blue")
+        ellipseMode(CENTER)
+        ellipse(this.body.position.x, this.body.position.y, 5)
+    }
+    updateY(){
+        if(this.body.position.y>height){
+            Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,300)})
+        }
+    }
+}
